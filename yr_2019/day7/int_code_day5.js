1b2cdc621f4678ae6e87388742f4bd69cd02deb4
Vismaya P S <vismayaps@Vismayas-MacBook-Pro.local>
2025-12-16 14:24:04 +0530

    updated day5 with multiply fn

--- yr_2019/day7/int_code_day5.js
+++ yr_2019/day7/int_code_day5.js
@@ -1,9 +1,13 @@
 // import { data } from "./input.js";
 
-const getMode = (value, base) => Math.floor((value / base) % 10);
+const getMode = (opCode) => {
+  const hundreds = Math.floor((opCode / 100) % 10);
+  const thousands = Math.floor((opCode / 1000) % 10);
+  return [hundreds, thousands];
+};
 
 let computer = {
-  program: [1, 0, 0, 0, 99, 1, 0, 0, 0, 99],
+  program: [102, 5, 1, 0, 99, 1, 0, 0, 0, 99],
   isHalt: false,
   index: 0,
 };
@@ -13,21 +17,26 @@ const paramModes = {
   1: (program, index) => program[index],
 };
 
-const add = (program, index) => {
-  const hundreds = getMode(program[index], 100);
-  const thousands = getMode(program[index], 1000);
-  return paramModes[hundreds](program, index + 1) +
-    paramModes[thousands](program, index + 2);
-};
+const add = (a, b) => a + b;
+
+const mul = (a, b) => a * b;
 
 const executeInstruction = (computer) => {
   let { program, isHalt, index } = computer;
   while (!isHalt) {
-    const operation = program[index];
-    // console.log({ operation, index });
+    const operation = program[index] % 100;
+    const [hundreds, thousands] = getMode(program[index]);
+
+    const input1 = paramModes[hundreds](program, index + 1);
+    const input2 = paramModes[thousands](program, index + 2);
+
     const instructions = {
       1: () => {
-        program[program[index + 3]] = add(program, index);
+        program[program[index + 3]] = add(input1, input2);
+        return index += 4;
+      },
+      2: () => {
+        program[program[index + 3]] = mul(input1, input2);
         return index += 4;
       },
       99: () => isHalt = true,
